f2a5862334f280ed68b9ea0d027fcebae9b231fa
LAURIDS0 <66024255+LAURIDS0@users.noreply.github.com>
2025-06-23 22:17:57 +0000

    Test commit message

---
+++ fxmanifest.lua
@@ -0,0 +1,31 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+author 'LC Development'
+description 'An advanced phone system for FiveM with various features and a user-friendly interface.'
+version '1.3.0'
+
+ui_page 'html/index.html'
+
+shared_scripts {
+    'config.lua'
+}
+
+client_scripts {
+    'client/main.lua',
+    'client/animation.lua'
+}
+
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+    'server/main.lua'
+}
+
+files {
+    'html/*.html',
+    'html/js/*.js',
+    'html/img/*.png',
+    'html/css/*.css',
+    'html/img/backgrounds/*.png',
+    'html/img/apps/*.png',
+}
